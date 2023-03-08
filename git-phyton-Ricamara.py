fe509f844ee0529eec645202fa6bedb72acc4739
Ricamara Gilbert <ricamaragilbert4@gmail>
2023-03-08 17:49:54 +0800

    My Personal Info added

---
+++ git-phyton-Ricamara.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 17:42:50 2023
+
+@author: Gilbert
+"""
+
+print("Gilbert o. Ricamara")
+print("Makawayan Torrijos Marinduque")
+print("November 8, 2002")
