ae906bcafbfc3f8117c2d6edf7b25289f21476a9
Ricamara Gilbert <ricamaragilbert4@gmail>
2023-03-08 17:58:56 +0800

    MyEducationalBackground

--- git-phyton-Ricamara.py
+++ git-phyton-Ricamara.py
@@ -8,3 +8,6 @@ Created on Wed Mar  8 17:42:50 2023
 print("Gilbert o. Ricamara")
 print("Makawayan Torrijos Marinduque")
 print("November 8, 2002")
+print("BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY")
+print("2nd YEAR")
+print("class-C")
